bddc8a28857140e1d8b6a59442f0de173a056575
Danny Armijos <daniel2667@hotmail.es>
2024-02-04 00:23:20 +0100

    first commit

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Mensaje from "./Mensaje";
+
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+  const [mensaje, setMensaje] = useState("");
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+    if (!Number(presupuesto) || Number(presupuesto) < 0) {
+      setMensaje("No es un presupuesto valido");
+    } else {
+      console.log("es un numero");
+    }
+  };
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+      <form onSubmit={handlePresupuesto} className="formulario">
+        <div className="campo">
+          <label htmlFor="nuevo-presupuesto">Definir presupuesto</label>
+          <input
+            className="nuevo-presupuesto"
+            type="text"
+            name=""
+            id="nuevo-presupuesto"
+            placeholder="Añade tu presupuesto"
+            value={presupuesto}
+            onChange={(e) => setPresupuesto(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Añadir" />
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+      </form>
+    </div>
+  );
+};
+
+export default NuevoPresupuesto;
